c7db1f6c4d3cfae88b7142b045583aaf7d551229
victorzarzar <victorzarzar58@gmail.com>
2025-07-17 07:51:58 -0300

    Add components and utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,70 @@
+import 'package:cltvspj/controller/calculator_controller.dart';
+import 'package:cltvspj/controller/locale_controller.dart';
+import 'package:cltvspj/controller/notification_controller.dart';
+import 'package:cltvspj/features/theme_provider.dart';
+import 'package:cltvspj/services/notification_service.dart';
+import 'package:cltvspj/services/secure_service.dart';
 import 'package:cltvspj/views/home_page.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:provider/provider.dart';
+import 'package:timezone/data/latest_10y.dart' as tz;
 
-void main() {
-  return runApp(ModularApp(module: AppModule(), child: AppWidget()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  await NotificationService.init();
+  tz.initializeTimeZones();
+  SecureStorageService.init();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('pt', 'BR'),
+        Locale('es'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => NotificationController()),
+          ChangeNotifierProvider(create: (_) => LocaleController()),
+          ChangeNotifierProvider(create: (_) => CalculatorController()),
+          ChangeNotifierProvider(create: (_) => UiProvider()..init()),
+        ],
+        child: ModularApp(module: AppModule(), child: const AppWidget()),
+      ),
+    ),
+  );
 }
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
 
   @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'My Smart App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      routerConfig: Modular.routerConfig,
-    ); //added by extension
+    return Consumer<UiProvider>(
+      builder: (context, UiProvider notifier, child) {
+        return MaterialApp.router(
+          title: 'CLT VS PJ',
+          debugShowCheckedModeBanner: false,
+          theme: notifier.lightTheme,
+          darkTheme: notifier.darkTheme,
+          themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
+          locale: context.locale,
+          supportedLocales: context.supportedLocales,
+          localizationsDelegates: context.localizationDelegates,
+          routerConfig: Modular.routerConfig,
+        );
+      },
+    );
   }
 }
 
@@ -26,6 +74,6 @@ class AppModule extends Module {
 
   @override
   void routes(r) {
-    r.child('/', child: (context) => HomePage());
+    r.child('/', child: (context) => const HomePage());
   }
 }
---
+++ lib/views/widgets/body_home_container.dart
@@ -0,0 +1,166 @@
+import 'package:cltvspj/controller/calculator_controller.dart';
+import 'package:cltvspj/features/app_theme.dart';
+import 'package:cltvspj/features/theme_provider.dart';
+import 'package:cltvspj/views/components/custom_button.dart';
+import 'package:cltvspj/views/components/input_field.dart';
+import 'package:cltvspj/views/components/show_dialog_error.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BodyContainer extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  final TextEditingController salaryCltController;
+  final TextEditingController salaryPjController;
+  final TextEditingController benefitsController;
+  final TextEditingController taxesPjController;
+  final TextEditingController accountantFeeController;
+  final TextEditingController inssPjController;
+  final VoidCallback onCalculatePressed;
+  final double padding;
+  final double minHeight;
+  final double maxWidth;
+
+  const BodyContainer({
+    super.key,
+    required this.formKey,
+    required this.salaryCltController,
+    required this.salaryPjController,
+    required this.benefitsController,
+    required this.taxesPjController,
+    required this.accountantFeeController,
+    required this.inssPjController,
+    required this.onCalculatePressed,
+    required this.padding,
+    required this.minHeight,
+    required this.maxWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = context.read<CalculatorController>();
+    return Consumer<UiProvider>(
+      builder: (context, notifier, child) {
+        return Center(
+          child: GestureDetector(
+            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(padding),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: maxWidth,
+                  minHeight: minHeight,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(padding),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: notifier.isDark
+                                ? CardColor.primaryColor
+                                : CardColor.secondaryColor,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: notifier.isDark
+                                    ? Colors.black.withValues(alpha: 0.3)
+                                    : Colors.grey.withValues(alpha: 0.2),
+                                blurRadius: 20,
+                                offset: const Offset(0, 10),
+                                spreadRadius: 0,
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(24.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                InputField(
+                                  label: 'salary_clt'.tr(),
+                                  controller: controller.salaryCltController,
+                                  icon: Icons.attach_money_rounded,
+                                  maxWidth: maxWidth,
+                                  onChanged: (_) => controller.calculate(),
+                                ),
+                                InputField(
+                                  label: 'salary_pj'.tr(),
+                                  controller: controller.salaryPjController,
+                                  icon: Icons.business_center_rounded,
+                                  maxWidth: maxWidth,
+                                  onChanged: (_) => controller.calculate(),
+                                ),
+                                InputField(
+                                  label: 'benefits_clt'.tr(),
+                                  controller: benefitsController,
+                                  icon: Icons.card_giftcard_rounded,
+                                  maxWidth: maxWidth,
+                                  onChanged: (_) => controller.calculate(),
+                                ),
+                                InputField(
+                                  label: 'accountant_fee'.tr(),
+                                  controller:
+                                      controller.accountantFeeController,
+                                  icon: Icons.receipt_long_rounded,
+                                  maxWidth: maxWidth,
+                                  onChanged: (_) => controller.calculate(),
+                                ),
+                                InputField(
+                                  label: 'inss_pj'.tr(),
+                                  controller: controller.inssPjController,
+                                  icon: Icons.percent_rounded,
+                                  maxWidth: maxWidth,
+                                  onChanged: (_) => controller.calculate(),
+                                ),
+                                InputField(
+                                  label: 'taxes_pj'.tr(),
+                                  controller: controller.taxesPjController,
+                                  icon: Icons.account_balance_rounded,
+                                  maxWidth: maxWidth,
+                                  onChanged: (_) => controller.calculate(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        CustomButton(
+                          animatedGradient: true,
+                          fullWidth: true,
+                          height: 42,
+                          maxWidth: maxWidth,
+                          color: notifier.isDark
+                              ? ButtonColor.fourthColor
+                              : ButtonColor.primaryColor,
+                          onPressed: () {
+                            final controller = context
+                                .read<CalculatorController>();
+
+                            if (!controller.hasValidInput) {
+                              ShowDialogError.show(
+                                context,
+                                title: 'error'.tr(),
+                                child: Text('fill_fields_to_see_chart'.tr()),
+                              );
+                              return;
+                            }
+                            onCalculatePressed();
+                          },
+                          text: 'calculate'.tr(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
