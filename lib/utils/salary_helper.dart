b5c561c115135b21498f5d27c1b112b96f6499b3
victorzarzar <victorzarzar58@gmail.com>
2026-01-23 16:15:17 -0300

    Chore: Update salary hellper and add new package

--- lib/utils/salary_helper.dart
+++ lib/utils/salary_helper.dart
@@ -1,37 +1,53 @@
 double calculateInss(double grossSalary) {
-  if (grossSalary <= 1518.00) {
+  if (grossSalary <= 1621.00) {
     return grossSalary * 0.075;
-  } else if (grossSalary <= 2793.88) {
-    return (1518.00 * 0.075) + ((grossSalary - 1518.00) * 0.09);
-  } else if (grossSalary <= 4190.83) {
-    return (1518.00 * 0.075) +
-        ((2793.88 - 1518.00) * 0.09) +
-        ((grossSalary - 2793.88) * 0.12);
-  } else if (grossSalary <= 8157.41) {
-    return (1518.00 * 0.075) +
-        ((2793.88 - 1518.00) * 0.09) +
-        ((4190.83 - 2793.88) * 0.12) +
-        ((grossSalary - 4190.83) * 0.14);
+  } else if (grossSalary <= 3061.00) {
+    return (1621.00 * 0.075) + ((grossSalary - 1621.00) * 0.09);
+  } else if (grossSalary <= 6101.06) {
+    return (1621.00 * 0.075) +
+        ((3061.00 - 1621.00) * 0.09) +
+        ((grossSalary - 3061.00) * 0.12);
+  } else if (grossSalary <= 8475.55) {
+    return (1621.00 * 0.075) +
+        ((3061.00 - 1621.00) * 0.09) +
+        ((6101.06 - 3061.00) * 0.12) +
+        ((grossSalary - 6101.06) * 0.14);
   } else {
-    return 1236.70;
+    return (1621.00 * 0.075) +
+        ((3061.00 - 1621.00) * 0.09) +
+        ((6101.06 - 3061.00) * 0.12) +
+        ((8475.55 - 6101.06) * 0.14);
   }
 }
 
 double calculateIrrf(double grossSalary) {
-  final inss = calculateInss(grossSalary);
-  final base = grossSalary - inss;
+  final inssDeduction = calculateInss(grossSalary);
+  const simplifiedDiscount = 607.20;
+  final taxBase =
+      grossSalary -
+      (inssDeduction > simplifiedDiscount ? inssDeduction : simplifiedDiscount);
 
-  if (base <= 3036.00) {
-    return 0.0;
-  } else if (base <= 4214.12) {
-    return (base * 0.075) - 227.70;
-  } else if (base <= 5282.45) {
-    return (base * 0.15) - 556.02;
-  } else if (base <= 6765.66) {
-    return (base * 0.225) - 922.57;
+  double irrf = 0.0;
+
+  if (taxBase <= 2428.80) {
+    irrf = 0.0;
+  } else if (taxBase <= 3227.73) {
+    irrf = (taxBase * 0.075) - 182.16;
+  } else if (taxBase <= 4087.87) {
+    irrf = (taxBase * 0.15) - 394.16;
+  } else if (taxBase <= 5052.87) {
+    irrf = (taxBase * 0.225) - 675.49;
   } else {
-    return (base * 0.275) - 1265.98;
+    irrf = (taxBase * 0.275) - 908.73;
   }
+
+  if (grossSalary <= 7350.00 && irrf > 0) {
+    const maxReduction = 312.89;
+    final reduction = irrf < maxReduction ? irrf : maxReduction;
+    irrf = irrf - reduction;
+  }
+
+  return irrf < 0 ? 0.0 : irrf;
 }
 
 double calculateFgts(double grossSalary) {
